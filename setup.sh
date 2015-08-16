16a10f735e7f44bf53b203fb5d89ab15af1eea0c
kindsenior <kunio.einstein@gmail.com>
2015-08-17 03:08:57 +0900

    add udev setting

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+echo '\nKERNEL=="ttyUSB*", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", MODE="0666"' | sudo cat >> /etc/udev/rules.d/50-udev.rules
+
 sudo ln -sf ~/git/Arduino-Makefile/Arduino.mk /usr/share/arduino/
 
 # perl cpan install 
